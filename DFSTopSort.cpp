23699485b7ba1ce420f26d8f66b437e5908c44f8
Matej Kroflin <matej.kroflin@gmail.com>
2017-10-13 02:22:25 +0200

    Topological Sort

---
+++ DFSTopSort.cpp
@@ -0,0 +1,57 @@
+// Topological Sort on DAG
+//
+// Prints out one topological sort for a given DAG (Directed Acyclic Graph).
+//
+// Variables:
+// - G is the adjecency list of the graph.
+// - P[x] is true if vertex x has been visited.
+// - Sorted is the deque of topologicaly ordered vertices.
+// - V, E are the number of vertices and edges in the graph.
+// 
+// Functions:
+// - dfs(D, u) does a dfs from vertex u.
+//
+// Time complexity: O(E)
+
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+vvi G;
+vi P;
+
+int dfs(deque<int> &D, int u)
+{
+    for (int v : G[u])
+        if (!P[v])
+            P[v] = 1, dfs(D, v);
+
+    D.push_front(u);
+}
+
+int main()
+{
+    int V, E;
+    G.assign(V, vi());
+    cin >> V >> E;
+    for (int i = 0; i < E; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back(v);
+    }
+
+    deque<int> Sorted;
+    P.assign(V, 0);
+    for (int i = 0; i < V; ++i)
+        if (!P[i])
+            dfs(Sorted, i);
+    
+    for (int i : Sorted)
+        cout << i << ' ';
+}
