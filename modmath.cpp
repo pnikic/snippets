13bb22ea2bce8968d7c33adfec7a3d89b98b006f
Matej Kroflin <matej.kroflin@gmail.com>
2017-12-26 00:08:14 +0100

    nCr

---
+++ modmath.cpp
@@ -0,0 +1,81 @@
+// Binomial coefficient
+// 
+// Calculating nCr (n choose r).
+//
+// Variables:
+// - mod is the module
+// - maxn is the largest possible n
+// - f[n] = n!
+//
+// Functions:
+// - powmod(a, k) calculates a ^ k % mod
+// - invmod(a) calculates inverse of a modulo mod
+// - precompute() precomputes factorials up to maxn
+// - nCr(n, r) calculates n choose r 
+// 
+// Time complexities:
+// - powmod: O(log(k))
+// - invmod: O(log(mod))
+// - nCr: O(log(mod))
+// - precompute: O(N)
+//
+// Note: before using nCr call precompute() to precompute factorials
+
+
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+const ll mod = 1000000009;
+const ll maxn = 10000;
+
+ll f[maxn];
+
+ll powmod(ll a, ll k)
+{
+    ll res = 1;
+    while (k)
+    {
+        if (k % 2)
+            res = (res * a) % mod;
+        
+        a = (a * a) % mod;
+        k /= 2;
+    }
+
+    return res;
+}
+
+ll invmod(ll a)
+{
+    return powmod(a, mod - 2);
+}
+
+ll nCr(ll n, ll r)
+{
+    return (f[n] * ((invmod(f[r]) * invmod(f[n - r])) % mod)) % mod;
+}
+
+void precompute()
+{
+    f[0] = 1;
+    for (ll i = 1; i < maxn; ++i)
+        f[i] = i * f[i - 1];
+}
+        
+int main()
+{
+    precompute();
+    for (int i = 0; i < 10; ++i)
+        cout << f[i] << endl;
+    
+    for (int i = 1; i < 10; ++i)
+    {
+        for (int j = 0; j <= i; ++ j)
+            cout << nCr(i, j) << ' ';
+        
+        cout << endl;
+    }
+}
