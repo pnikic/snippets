1ffe0bba8ea3d65207abb6e48c28e492da33c671
Mislav Blažević <mislav.blazevic5@gmail.com>
2018-11-24 16:29:33 +0100

    Update FenwickTree

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -22,27 +22,31 @@ typedef vector<int> vi;
 
 class FenwickTree
 {
-    private:
-        vi ft;
-    public:
-        FenwickTree (int n)
-        {
-            ft.assign(n + 1, 0);
-        }
-        int rsq(int b)
-        {
-            int sum = 0;
-            for (; b; b -= (b & (-b)))
-                sum += ft[b];
-            return sum;
-        }
-        int rsq(int a, int b)
-        {
-            return rsq(b) - ((a == 1)? 0 : rsq(a - 1));
-        }
-        void adjust(int k, int v)
-        {
-            for (; k < ft.size(); k += (k & (-k)))
-                ft[k] += v;
-        }
+private:
+    vi ft;
+public:
+    FenwickTree (int n) : ft(n + 1)
+    {
+    }
+    int rsq(int b)
+    {
+        int sum = 0;
+        for (; b; b -= (b & (-b)))
+            sum += ft[b];
+        return sum;
+    }
+    int rsq(int a, int b)
+    {
+        return rsq(b) - rsq(a);
+    }
+    void adjust(int k, int v)
+    {
+        for (; k < ft.size(); k += (k & (-k)))
+            ft[k] += v;
+    }
+    void adjust(int l, int r, int v)
+    {
+        adjust(l, v);
+        adjust(r + 1, -v);
+    }
 };
