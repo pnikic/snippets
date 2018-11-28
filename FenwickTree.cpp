cea1b8e4d56b525ab11362260fbe5388b42c1447
DDTantor <matej.kroflin@gmail.com>
2018-11-28 10:44:14 +0100

    added range update fenwick

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -2,11 +2,17 @@
 //
 // Dynamic data structure used to solve problems such as
 // RSQ (Range Sum Queries) given a set of M integer keys that range
-// from [1 ... N].
+// from [1 ... N]. RangeFT allows range updates and queries in
+// log N time.
 //
 // Functions:
-// - rsq() returns the RSQ.
-// - adjust(k, N) update.
+// Class FenwickTree
+// - rsq(a, b) returns the RSQ from a to b (inclusive).
+// - adjust(N, v) add v to the Nth element.
+// - q(N) returns Nth element.
+// Class RangeFT
+// - rsq(a, b) returns the RSQ from a to b (inclusive).
+// - adjust(l, r, v) add v to elements from a to b (inclusive);
 //
 // Time complexities:
 // - rsq: O(log N)
@@ -14,7 +20,6 @@
 
 #include <iostream>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
@@ -24,6 +29,7 @@ class FenwickTree
 private:
     vector<T> ft;
 public:
+    FenwickTree () {}
     FenwickTree (int n) : ft(n + 1)
     {
     }
@@ -47,9 +53,30 @@ public:
         for (; k < ft.size(); k += (k & (-k)))
             ft[k] += v;
     }
+};
+
+template<class T>
+class RangeFT
+{
+private:
+    FenwickTree<T> mul, add;
+public:
+    RangeFT (int n) : mul(FenwickTree<T>(n)), add(FenwickTree<T>(n))
+    {
+    }
+    T rsq(int a)
+    {
+        return mul.rsq(a) * a + add.rsq(a);
+    }
+    T rsq(int a, int b)
+    {
+        return rsq(b) - rsq(a - 1);
+    }
     void adjust(int l, int r, T v)
     {
-        adjust(l, v);
-        adjust(r + 1, -v);
+        mul.adjust(l, v);
+        mul.adjust(r, -v);
+        add.adjust(l, -v * (l - 1));
+        add.adjust(r, v * r);
     }
 };
